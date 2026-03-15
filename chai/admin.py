cc213a990381d3554260143a6497758d247aa7bb
Himanshu116k <himanshu1991patna@gmail.com>
2026-03-15 09:22:28 +0530

    final commit

--- chai/admin.py
+++ chai/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
-from .models import ChaiVarity
-from django.urls import path
+from .models import ChaiVarity,ChaiCertificate,ChaiReview,Store
 
 # Register your models here.
-admin.site.register(ChaiVarity)
+class ChaiReviewInline(admin.TabularInline):
+    model = ChaiReview
+    extra = 2
+
+class ChaiCertificateInline(admin.ModelAdmin):
+    list_display=('chai','certificate_no')
+
+class ChaiVarityAdmin(admin.ModelAdmin):
+    list_display = ('name','type','date_added')
+    list_filter = ('type','date_added') 
+    inlines = [ChaiReviewInline]
+    
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('name','location')
+    filter_horizontal = ('chai_varities',)
+
+admin.site.register(ChaiVarity,ChaiVarityAdmin) 
+admin.site.register(Store,StoreAdmin) 
+admin.site.register(ChaiCertificate,ChaiCertificateInline) 
+# admin.site.register(ChaiReview,ChaiReviewInline)
